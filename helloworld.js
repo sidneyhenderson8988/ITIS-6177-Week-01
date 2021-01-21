5fde62decd30e45f9e43f42b39b179d094784332
Sidney Henderson <shende39@uncc.edu>
2021-01-21 12:41:29 -0500

    Assignment 1 finished

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+// var http = require('http');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World!');
+// }).listen(3000);
+
+// NOTE: I wasn't sure if the professor wanted the "Hello World" example to be a server,
+// so I included that portion above. Below is the console.log() the professor
+// asked for in the video that explains Exercise 1.
+
+console.log("Hello World!")
